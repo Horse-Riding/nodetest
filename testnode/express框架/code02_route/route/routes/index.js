f9e9e33d46ddbbc1ac28102ae2e4c18937fbc81e
admin <449135633@qq.com>
2020-01-10 14:37:11 +0800

    save

--- testnode/express框架/code02_route/route/routes/index.js
+++ testnode/express框架/code02_route/route/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 编写login.html路由
+router.post('/login.html',function(req, res) {
+	var username = req.body.username;
+	var pwd = req.body.pwd;
+	res.send('您传的用户名是:' + username + ',您传的密码是:' + pwd);
+})
 module.exports = router;
